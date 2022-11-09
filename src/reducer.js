bc4a2b1fc0de7334869d9829387ea3fe7db2ca14
pnichols789 <pnichols789@gmail.com>
2022-11-09 10:36:01 -0600

    already added alert not working

--- src/reducer.js
+++ src/reducer.js
@@ -1,25 +1,36 @@
-function reducer(state=[], action){
-    if (action.type==='addTodo'){
-        return [...state, {id:state.length+1, title:action.description, completed:false}]
-    }
-    if(action.type==='removeTodo'){
-        const newData=state.filter(item=>item.id!==action.id)
-        return newData
-    }
-    if(action.type==='complete'){
-        return state.map(item=>(item.id === action.id ? {...item, completed: true} : item))
+function reducer(state = [], action) {
+	if (action.type === "addTodo") {
+		for (let i of state) {
+			if (i.title === action.title) {
+				alert("already added");
+				return state;
+			}
+		}
+		return [
+			...state,
+			{ id: state.length + 1, title: action.description, completed: false },
+		];
+	}
+	if (action.type === "removeTodo") {
+		const newData = state.filter((item) => item.id !== action.id);
+		return newData;
+	}
+	if (action.type === "complete") {
+		return state.map((item) =>
+			item.id === action.id ? { ...item, completed: true } : item
+		);
 
-        // const newData=[]
-        // for(let i of state){
-        //     if(i.id===action.id){
-        //         newData.push({...i, completed:true})
-        //     }else{
-        //         newData.push(i)
-        //     }
-        // }
-        // return newData
-    }
-    return state
+		// const newData=[]
+		// for(let i of state){
+		//     if(i.id===action.id){
+		//         newData.push({...i, completed:true})
+		//     }else{
+		//         newData.push(i)
+		//     }
+		// }
+		// return newData
+	}
+	return state;
 }
 
-export default reducer
+export default reducer;
